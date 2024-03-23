4d577a6a2311454d2b54f62f6d537c0a4332bcb1
Александр <poni.taw@yandex.ru>
2024-03-23 21:02:52 +0300

    =))))

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=E:\ЧГУ\6 семестр\Сети\Chat\env
+set VIRTUAL_ENV=D:\git\Chat\env
 
 if not defined PROMPT set PROMPT=$P$G
 
